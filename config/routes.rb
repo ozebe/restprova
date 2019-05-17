128617096dbd04f47fd9e32e78934228302d45be
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-05-16 22:22:10 -0300

    Criado o Scaffold do "ingredient".

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ingredients
   resources :dishes
   resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
