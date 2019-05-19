17ab85b360dc66c17a68591b9b67890eb94b5407
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-05-18 21:35:44 -0300

    Atualizado o controlador da "home" para ser possível pesquisar por categorias, possivelmente será modificado o modo de busca

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    @dishes = if params[:term]
+                Dish.where('category LIKE ?', "%#{params[:term]}%")
+              else
+                Dish.all
+              end
   end
 end
+
+
