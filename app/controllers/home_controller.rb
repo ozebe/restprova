839d5db8b16bd42af86f2d844eed56adc22da39b
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-05-19 15:17:25 -0300

    Melhorado o fator de pesquisa no home, onde é possível pesquisar pela categoria do prato e ele apresentará quais restaurantes possuem tal categoria

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,21 @@
 class HomeController < ApplicationController
   def index
+    #é criado a consulta sql que verifica se o restaurante possui algum prato com a categoria passada por get
     @restaurants = Restaurant.all
-    @dishes = if params[:term]
-                Dish.where('category LIKE ?', "%#{params[:term]}%")
+    @restaurants = @restaurants.distinct()
+    @restaurants = @restaurants.joins("INNER JOIN dishes_restaurants AS drs
+    ON restaurants.id = drs.restaurant_id INNER JOIN dishes AS di ON drs.dish_id = di.id")
+
+    @restaurants = if params[:term]
+                     @restaurants.where('UPPER (di.category) LIKE ? ', "#{params[:term].upcase}%").distinct
+
               else
-                Dish.all
+                @restaurants.all
               end
   end
+
+
+
 end
 
 
